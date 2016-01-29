7d62dd4c861ccfe5babf79439dc9e9a2f3b0cdb8
Spencer <spencer.p.816@gmail.com>
2016-01-29 14:37:54 -0800

    Complete exercise1 (except for bonus)

--- exercise1.R
+++ exercise1.R
@@ -7,40 +7,50 @@ library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
+nrow(flights)
+ncol(flights)
+dim(flights)
 
 # Add a column that is the amount of time gained in the air
-
+flights2 <- mutate(flights, gained_time = arr_delay - dep_delay)
 
 # Sort your data.frame desceding by the column you just created
-
+flights2 <- arrange(flights2, desc(gained_time))
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights2 <- flights2 %>% 
+            mutate(flights, gained_time = arr_delay - dep_delay) %>%
+            arrange(desc(gained_time))
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights2$gained_time)
 
 # On average, did flights gain or lose time?
-
+average_gain <- mean(flights2$gained_time, na.rm = TRUE)
 
 # Create a data.frame of flights headed to seatac ('SEA'), 
 # and only include the column you just created
-
+flights_sea <- flights2 %>% filter(dest == 'SEA')
 
 # On average, did flights to seatac gain or loose time?
-
+average_gain_sea <- mean(flights_sea$gained_time, na.rm = TRUE)
 
 # Write a function that allows you to specify an origin, a destination, and a column of interest
 # that returns a data.frame of flights from the origin to the destination and only the column of interest
 ## Hint: see slides on standard evaluation
-
+flight_info <- function(from, destination, col_interest) {
+   to_from <- flights2 %>% 
+             filter(dest == destination, origin == from) %>% 
+             select_(col_interest)
+   return(to_from)
+}
 
 # Retireve the air_time column for flights from JFK to SEA
-
+JFK_to_SEA_airtime <- flight_info('JFK', 'SEA', 'air_time')
 
 # What was the average air time of those flights (in hours)?  What was the min/max?
-
+mean(JFK_to_SEA_airtime$air_time, na.rm = TRUE)
 
 ### Bonus ###
 # Rewrite the function above to return a list of the min, max, and mean values of the column of interest
